fb6e313e709c64b726ffce7d327456afa2a5b1d1
wegi8 <18272190626@163.com>
2022-06-15 15:59:36 +0800

    refactor: update the `skeleton`

--- packages/varlet-lowcode-skeleton/src/Skeleton.tsx
+++ packages/varlet-lowcode-skeleton/src/Skeleton.tsx
@@ -1,7 +1,8 @@
+import type { Ref } from 'vue'
 import { defineComponent, ref, computed } from 'vue'
 import { AppBar, Icon, Space } from '@varlet/ui'
 import { pluginsManager } from '@varlet/lowcode-core'
-import { SkeletonLayouts } from '@varlet/lowcode-core/src/modules/plugins'
+import { SkeletonLayouts, SkeletonPlugin } from '@varlet/lowcode-core/src/modules/plugins'
 import '@varlet/ui/es/app-bar/style/index.js'
 import '@varlet/ui/es/icon/style/index.js'
 import '@varlet/ui/es/space/style/index.js'
@@ -12,25 +13,29 @@ export default defineComponent({
   setup() {
     return () => {
       const plugins = pluginsManager.exportSkeletonPlugins()
-      const pickerComponents = (layout: SkeletonLayouts) => {
-        const comps: Array<JSX.Element> = []
-        plugins.forEach((plugin) => {
-          if (plugin.layout === layout) {
-            if (layout.includes('header')) {
-              comps.push(plugin.component.render!())
-            }
 
-            if (layout.includes('sidebar')) {
-              if (typeof plugin.icon === 'string') {
-                comps.push(<Icon name={plugin.icon} on-click={() => changeCurrentComponent(plugin.name)} />)
-              } else {
-                const { icon } = plugin
-                comps.push(<icon onClick={() => changeCurrentComponent(plugin.name)} />)
-              }
-            }
-          }
-        })
-        return comps
+      const pickerComponents = (layout: SkeletonLayouts) => {
+        const _plugins: SkeletonPlugin[] = plugins.filter((plugin) => plugin.layout === layout)
+
+        if (!_plugins || _plugins.length === 0) {
+          throw new Error(`${layout} is not a valid layout`)
+        }
+
+        if (layout.includes('header')) {
+          return _plugins.map(({ component }: SkeletonPlugin) => component.render!())
+        }
+
+        if (layout.includes('sidebar')) {
+          return _plugins.map(({ icon, name }: SkeletonPlugin) =>
+            typeof icon === 'string' ? (
+              <Icon name={icon} on-click={() => changeCurrentComponent(name)} />
+            ) : (
+              <icon onClick={() => changeCurrentComponent(name)} />
+            )
+          )
+        }
+
+        return []
       }
 
       const Header = () => {
@@ -49,54 +54,48 @@ export default defineComponent({
         )
       }
 
-      const currentComponentName = ref('')
+      const currentComponentName: Ref<string | undefined> = ref('')
+
       const changeCurrentComponent = (name: string) => {
-        currentComponentName.value = name === currentComponentName.value ? '' : name
+        currentComponentName.value = name === currentComponentName.value ? undefined : name
       }
+
       const currentComponent = computed(() => {
-        return plugins.find((plugin) => plugin.name === currentComponentName.value)!.component
+        if (!currentComponentName.value) return null
+        const PluginComponent = plugins.find((plugin) => plugin.name === currentComponentName.value)!.component
+        return (
+          <div style="width: 200px">
+            <PluginComponent />
+          </div>
+        )
       })
 
       const SideBar = () => {
         const Top = pickerComponents(SkeletonLayouts.SIDEBAR_TOP)
         const Bottom = pickerComponents(SkeletonLayouts.SIDEBAR_BOTTOM)
         return (
-          <div class={'sidebar'}>
-            <Space direction={'column'} align={'center'} justify={'space-between'}>
+          <div className="sidebar">
+            <Space direction="column" align="center" justify="space-between">
               {Top}
               {Bottom}
             </Space>
-            {currentComponentName.value ? (
-              <div
-                style={{
-                  width: '200px',
-                }}
-              >
-                <currentComponent.value />
-              </div>
-            ) : (
-              ''
-            )}
+            {currentComponent.value}
           </div>
         )
       }
 
-      const DrawingBoard = () => {
-        return <div class={'drawing-board'} />
-      }
-
       const Container = () => {
         return (
-          <div class={'container'}>
+          <div class="container">
             <SideBar />
-            <DrawingBoard />
+            <div class="drawing-board" />
           </div>
         )
       }
 
       return (
-        <div class={'main'}>
-          <div class={'skeleton'}>
+        <div class="main">
+          <div class="skeleton">
             <Header />
             <Container />
           </div>
