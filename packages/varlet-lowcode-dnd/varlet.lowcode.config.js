a7483d6dbcd855821c6fda37a91e43a8d1583847
wegi8 <18272190626@163.com>
2022-06-29 18:13:16 +0800

    chore(dnd): update the copy plugin

--- packages/varlet-lowcode-dnd/varlet.lowcode.config.js
+++ packages/varlet-lowcode-dnd/varlet.lowcode.config.js
@@ -2,19 +2,23 @@ const { ensureDirSync, copySync } = require('fs-extra')
 
 module.exports = {
   name: 'varlet-lowcode-dnd',
-  plugins: [
-    {
-      name: 'copy-plugin',
-      apply: 'build',
-      closeBundle() {
-        if (process.env.COMMAND === 'compile') {
-          ensureDirSync('../varlet-lowcode-renderer/public')
-          copySync(
-            'lib/varlet-lowcode-dnd.umd.js',
-            '../varlet-lowcode-renderer/public/varlet-lowcode-dnd.umd.js'
-          )
-        }
-      },
-    },
-  ],
+
+  configureVite(command) {
+    if (command === 'compile') {
+      return {
+        plugins: [
+          {
+            name: 'copy-plugin',
+            apply: 'build',
+            closeBundle() {
+              if (process.env.COMMAND === 'compile') {
+                ensureDirSync('../varlet-lowcode-renderer/public')
+                copySync('lib/varlet-lowcode-dnd.umd.js', '../varlet-lowcode-renderer/public/varlet-lowcode-dnd.umd.js')
+              }
+            },
+          },
+        ],
+      }
+    }
+  },
 }
