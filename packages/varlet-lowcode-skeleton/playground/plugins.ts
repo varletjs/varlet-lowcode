58f733ab5dc4e8271364eed89a3a082137b2638f
qytayh <13255238145@163.com>
2022-07-08 10:01:03 +0800

    revert: refactor(core/plugins): remove SkeletonPlugin's & SelectorPlugin's name

--- packages/varlet-lowcode-skeleton/playground/plugins.ts
+++ packages/varlet-lowcode-skeleton/playground/plugins.ts
@@ -11,34 +11,41 @@ export function usePlugins() {
   pluginsManager
     .useSkeletonPlugin({
       layout: SkeletonLayouts.DESIGNER,
+      name: 'designer',
       component: Designer,
     })
     .useSkeletonPlugin({
       layout: SkeletonLayouts.SETTERS,
+      name: 'setters',
       component: Setters,
     })
     .useSkeletonPlugin({
       layout: SkeletonLayouts.HEADER_CENTER,
+      name: 'resizer',
       component: Resizer,
     })
     .useSkeletonPlugin({
       layout: SkeletonLayouts.SIDEBAR_TOP,
+      name: 'code-editor',
       component: CodeEditor,
       icon: 'xml',
       label: '源码面板',
     })
     .useSkeletonPlugin({
       layout: SkeletonLayouts.SIDEBAR_BOTTOM,
+      name: 'schema-editor',
       component: SchemaEditor,
       icon: 'code-json',
       label: 'SCHEMA 面板',
     })
     .useSkeletonPlugin({
-      layout: SkeletonLayouts.HEADER_RIGHT,
+      layout: SkeletonLayouts.HEADER_CENTER,
+      name: 'undo-repo',
       component: UndoRedo,
     })
     .useSkeletonPlugin({
       layout: SkeletonLayouts.HEADER_RIGHT,
+      name: 'codegen',
       component: Codegen,
       componentProps: {
         style: {
