2dc5213031e633014ffe842e28c84e7068dd2dab
haoziqaq <357229046@qq.com>
2022-07-12 00:19:51 +0800

    refactor: optimize package dep

--- packages/varlet-lowcode-designer/varlet.lowcode.config.js
+++ packages/varlet-lowcode-designer/varlet.lowcode.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   name: 'varlet-lowcode-designer',
+
+  configureVite(command, config) {
+    if (command === 'compile') {
+      config.build.lib.formats = ['es']
+    }
+  },
 }
