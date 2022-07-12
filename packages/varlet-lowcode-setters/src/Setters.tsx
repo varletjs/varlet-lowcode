4f7ef02c0cd103acba528d4aceaeff067da24f5a
chenjie <540053866@qq.com>
2022-07-12 19:53:53 +0800

    refactor(setters): update setter-tabs resize

--- packages/varlet-lowcode-setters/src/Setters.tsx
+++ packages/varlet-lowcode-setters/src/Setters.tsx
@@ -1,20 +1,36 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onUpdated, reactive } from 'vue'
 import HeaderTabs from './settersPlugins/headerTabs'
 import Empty from './empty'
 import './index.less'
 
-const isSelectDom = ref(false)
-
-isSelectDom.value = true
 export default defineComponent({
   name: 'VarletLowCodeSelector',
   setup() {
+    const isSelectDom = ref(false)
+    isSelectDom.value = true
+    type stateType = {
+      childRef: any
+    }
+
+    const state: stateType = reactive({
+      childRef: null,
+    })
+    onUpdated(() => {
+      state.childRef?.swipe.resize()
+    })
+
     return () => {
       return (
         <div class="varlet-lowcode-setters">
           <div class="varlet-lowcode-setters-content">
             {/* {isSelectDom.value ? <HeaderPath /> : null} */}
-            {isSelectDom.value ? <HeaderTabs /> : null}
+            {isSelectDom.value ? (
+              <HeaderTabs
+                refCallback={(ref: any) => {
+                  state.childRef = ref
+                }}
+              />
+            ) : null}
             {isSelectDom.value ? null : <Empty />}
           </div>
         </div>
