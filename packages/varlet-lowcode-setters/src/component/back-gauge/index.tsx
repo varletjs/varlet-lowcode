efb5db21adc8c8f21f7bf819a92234c0678769fd
jiechen66 <540053866@qq.com>
2022-08-02 16:42:29 +0800

    refactor(setters): update setter-back-gauge

--- packages/varlet-lowcode-setters/src/component/back-gauge/index.tsx
+++ packages/varlet-lowcode-setters/src/component/back-gauge/index.tsx
@@ -7,7 +7,10 @@ export default defineComponent({
     modelValue: {
       type: Object,
       default: () => {
-        return {}
+        return {
+          margin: {},
+          padding: {},
+        }
       },
     },
     positionValue: {
@@ -16,45 +19,57 @@ export default defineComponent({
     },
   },
   setup(props, { emit }) {
-    const positionVal: Record<string, any> = reactive({
-      left: '',
-      top: '',
-      right: '',
-      bottom: '',
+    const boxData: any = reactive({
+      margin: {
+        left: '',
+        top: '',
+        right: '',
+        bottom: '',
+      },
+      padding: {
+        left: '',
+        top: '',
+        right: '',
+        bottom: '',
+      },
     })
-    const inputContent = (val: any, str: string) => {
+    const inputContent = (val: any, str: string, type: string) => {
       const obj: Record<string, any> = {}
       obj[str] = val.target.value.length * 7.5
-      if (obj[str] < 30) {
-        obj[str] = 30
-      } else if (obj[str] > 200) {
-        obj[str] = 200
+      if (obj[str] < 20) {
+        obj[str] = 20
+      } else if (obj[str] > 120) {
+        obj[str] = 120
       }
       if (str === 'right' || str === 'left') {
-        document.documentElement.style.setProperty(`--position-theme-${str}-width`, obj[str] + 'px')
+        document.documentElement.style.setProperty(
+          `--${type === 'margin' ? 'margin' : 'padding'}-theme-${str}-width`,
+          obj[str] + 'px'
+        )
       }
     }
-    const blurInput = async (val: any, str: string) => {
+    const blurInput = async (val: any, str: string, type: string) => {
+      console.log(boxData[type], ' boxData[type]')
       if (!Number.isNaN(Number(val.target.value)) && val.target.value !== '') {
-        positionVal[str] = Number(val.target.value) + 'px'
+        boxData[type][str] = Number(val.target.value) + 'px'
       } else {
-        positionVal[str] = val.target.value
+        boxData[type][str] = val.target.value
       }
       if (val.target.value !== '') {
         const newData = JSON.parse(JSON.stringify(props.modelValue))
-        newData[str] = JSON.parse(JSON.stringify(positionVal[str]))
+        newData[type][str] = JSON.parse(JSON.stringify(boxData[type][str]))
         emit('update:modelValue', newData)
       }
       await nextTick()
-      positionVal[str] = ''
+      boxData[type][str] = ''
       val.target.value = ''
-      inputContent(val, str)
+      inputContent(val, str, type)
     }
     return () => {
       return (
         <div style={{ width: '100%' }}>
           <div class="position-value">
-            <span class="position-value__title">{props.positionValue}:</span>
+            <span class="position-value__title">margin:</span>
             {Object.keys(props.modelValue).map((item) => {
               return props.modelValue[item] === '' ? (
                 <span style={{ marginRight: '10px' }}>auto</span>
@@ -63,48 +78,76 @@ export default defineComponent({
               )
             })}
           </div>
-          <div class="layout-box-container">
-            <div class="margin-top-div">
-              <span class="explain-txt">MARGIN</span>
+          <div class="position-value">
+            <span class="position-value__title">padding:</span>
+            {Object.keys(props.modelValue).map((item) => {
+              return props.modelValue[item] === '' ? (
+                <span style={{ marginRight: '10px' }}>auto</span>
+              ) : (
+                <span style={{ marginRight: '10px' }}>{props.modelValue[item]}</span>
+              )
+            })}
+          </div>
+          <div class="varlet-low-code__box-container">
+            <div class="varlet-low-code__margin-top-div">
+              <span class="varlet-low-code__explain-txt">MARGIN</span>
               <span class="next-input">
-                <input type="text" />
+                <input type="text" onBlur={(e) => blurInput(e, 'top', 'margin')} />
               </span>
             </div>
-            <div class="margin-right-div">
+            <div class="varlet-low-code__margin-right-div">
               <span class="next-input">
-                <input type="text" />
+                <input
+                  type="text"
+                  onInput={(e) => inputContent(e, 'right', 'margin')}
+                  onBlur={(e) => blurInput(e, 'right', 'margin')}
+                />
               </span>
             </div>
-            <div class="margin-bottom-div">
+            <div class="varlet-low-code__margin-bottom-div">
               <span class="next-input">
-                <input type="text" />
+                <input type="text" onBlur={(e) => blurInput(e, 'bottom', 'margin')} />
               </span>
             </div>
-            <div class="margin-left-div">
+            <div class="varlet-low-code__margin-left-div">
               <span class="next-input">
-                <input type="text" />
+                <input
+                  type="text"
+                  onInput={(e) => inputContent(e, 'left', 'margin')}
+                  onBlur={(e) => blurInput(e, 'left', 'margin')}
+                />
               </span>
             </div>
-            <div class="padding-top-div">
-              <span class="explain-txt">PADDING</span>
-              <span class="next-input">
-                <input type="text" />
-              </span>
-            </div>
-            <div class="padding-right-div">
-              <span class="next-input">
-                <input type="text" />
-              </span>
-            </div>
-            <div class="padding-bottom-div">
-              <span class="next-input">
-                <input type="text" />
-              </span>
-            </div>
-            <div class="padding-left-div">
-              <span class="next-input">
-                <input type="text" />
-              </span>
+            <div class="varlet-low-code__padding">
+              <div class="varlet-low-code__padding-top-div">
+                <span class="varlet-low-code__explain-txt">PADDING</span>
+                <span class="next-input">
+                  <input type="text" onBlur={(e) => blurInput(e, 'top', 'padding')} />
+                </span>
+              </div>
+              <div class="varlet-low-code__padding-right-div">
+                <span class="next-input">
+                  <input
+                    type="text"
+                    onInput={(e) => inputContent(e, 'right', 'padding')}
+                    onBlur={(e) => blurInput(e, 'right', 'padding')}
+                  />
+                </span>
+              </div>
+              <div class="varlet-low-code__padding-bottom-div">
+                <span class="next-input">
+                  <input type="text" onBlur={(e) => blurInput(e, 'bottom', 'padding')} />
+                </span>
+              </div>
+              <div class="varlet-low-code__padding-left-div">
+                <span class="next-input">
+                  <input
+                    type="text"
+                    onInput={(e) => inputContent(e, 'left', 'padding')}
+                    onBlur={(e) => blurInput(e, 'left', 'padding')}
+                  />
+                </span>
+              </div>
             </div>
           </div>
         </div>
