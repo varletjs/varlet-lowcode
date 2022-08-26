2e24ce770f3a7b4bdd475c3ab0aaa25d3a9b617c
chris-paul <18270890760@163.com>
2022-08-26 22:55:41 +0800

    fix: designer rerender when resize

--- packages/varlet-lowcode-resizer/src/index.ts
+++ packages/varlet-lowcode-resizer/src/index.ts
@@ -2,6 +2,8 @@ import Resizer from './Resizer.vue'
 import { SkeletonLayouts } from '@varlet/lowcode-core'
 import type { SkeletonPlugin } from '@varlet/lowcode-core'
 
+export * from './types'
+
 const ResizerPlugin: SkeletonPlugin = {
   name: 'resizer',
   component: Resizer,
